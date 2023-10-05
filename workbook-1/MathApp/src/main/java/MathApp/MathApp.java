b463fc9fdc33bbaf7d3907668de1a85743a8aaed
Jada Harris <107131999+JadeZinHarris@users.noreply.github.com>
2023-10-05 14:14:06 -0400

    Completed Mod3 - Exercise 3 - Basic Calculator

---
+++ workbook-1/MathApp/src/main/java/MathApp/MathApp.java
@@ -0,0 +1,10 @@
+package MathApp;
+import java.util.Scanner;
+
+public class MathApp {
+    public static void main(String[] args) {
+        let bobSalary = 40,000
+                let garySalary = 60,000
+                let highgestSalary = Math.Max()
+    }
+}
